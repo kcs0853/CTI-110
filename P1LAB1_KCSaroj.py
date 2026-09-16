4871bb5394820b5251616fa4494d6cb64c5aedef
kcs0853 <kcs0853@student.faytechcc.edu>
2026-09-15 22:25:51 -0400

    first python assignment
    taking input from the user and printing output

---
+++ P1LAB1_KCSaroj.py
@@ -0,0 +1,10 @@
+# Saroj KC
+# September 15, 2026
+# Assignment Name: P1LAB1
+# Description: This program takes a user's first and last name as input and displays a welcome message.
+# input and print
+
+first_name = input("Enter your first name: ")
+last_name = input("Enter your last name: ")
+
+print("Hello,", first_name,last_name +"!", "Welcome to CTI-110!")
